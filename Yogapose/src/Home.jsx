43ebb1ea8ebeb219cb0936c56560ca3efdaa9a96
Anitto312 <anitto312@gmail.com>
2025-03-17 11:20:31 +0530

    m

--- Yogapose/src/Home.jsx
+++ Yogapose/src/Home.jsx
@@ -4,10 +4,10 @@ import { Link } from "react-router-dom";
 const Home=()=>{
     return(
         <div className="Home">
-            <p class="content-yoga">Lorem ipsum dolor sit amet consectetur, adipisicing elit. A totam, tempora repudiandae eius deserunt ipsa odio magnam non.<br/>
-             Harum provident consequuntur odio obcaecati quidem voluptates dolor ea enim itaque sunt!Quo officia commodi quasi nesciunt <br />
-             facilis voluptates sint cumque vitae placeat quisquam totam voluptas laudantium praesentium beatae dicta ducimus sit incidunt <br />
-              culpa tenetur aut, laboriosam atque asperiores! Aut, dolores explicabo?</p>
+            <p class="content-yoga">Yoga is an ancient practice that combines physical postures, breathing exercises, and meditation to promote overall well-being.<br/>
+            Originating in India over 5,000 years ago, yoga has evolved into various styles, each offering unique benefits for the mind and body.<br/>
+            Practicing yoga regularly can enhance flexibility, strength, and balance while reducing stress and improving mental clarity.<br/>
+            It is a holistic approach to health, connecting the body, mind, and spirit, providing a path to greater harmony and well-being.</p>
             <Link to='/Library'><button class="start">Get Started</button></Link>
         </div>
     )
