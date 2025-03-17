5a8c231970d13926898b0e74cbae4bf6f05db0d3
Anitto312 <anitto312@gmail.com>
2025-03-17 09:46:48 +0530

    1

--- yogabackend/server.js
+++ yogabackend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
+app.use(cors({ origin: ["https://yoga-pose-library.vercel.app/"], credentials: true }));
 
 const app = express();
 const PORT = process.env.PORT || 5000;
