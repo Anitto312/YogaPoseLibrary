1aea1fa27208b4a56f8001d5c06164ca99f50cfa
Anitto312 <anitto312@gmail.com>
2025-03-17 12:03:00 +0530

    m

--- Yogapose/src/Library.jsx
+++ Yogapose/src/Library.jsx
@@ -1,3 +1,23 @@
+// import "./css/Library.css"
+// const Library = () => {
+//   return (
+//     <div class="grid-container">
+//       <div class="grid-items"><button class="grid-button"><img src="/yoga1.png" alt="image-yoga" height="300px" width="300px"/></button><h4>yoga1</h4></div>
+//       <div class="grid-items"><button class="grid-button"><img src="/yoga2.png" alt="image-yoga" height="300px" width="300px"/></button><h4>yoga2</h4></div>
+//       <div class="grid-items"><button class="grid-button"><img src="/yoga3.png" alt="image-yoga" height="300px" width="300px"/></button><h4>yoga3</h4></div>
+//       <div class="grid-items"><button class="grid-button"><img src="/yoga4.png" alt="image-yoga" height="300px" width="300px"/></button><h4>yoga4</h4></div>
+//       <div class="grid-items"><button class="grid-button"><img src="/yoga5.png" alt="image-yoga" height="300px" width="300px"/></button><h4>yoga5</h4></div>
+//       <div class="grid-items"><button class="grid-button"><img src="/yog6.png" alt="image-yoga" height="300px" width="300px"/></button><h4>yoga6</h4></div>
+//       <div class="grid-items"><button class="grid-button"><img src="/yoga7.png" alt="image-yoga" height="300px" width="300px"/></button><h4>yoga7</h4></div>
+//       <div class="grid-items"><button class="grid-button"><img src="/yoga8.png" alt="image-yoga" height="300px" width="300px"/></button><h4>yoga8</h4></div>
+//       <div class="grid-items"><button class="grid-button"><img src="/yoga9.png" alt="image-yoga" height="300px" width="300px"/></button><h4>yoga9</h4></div>
+//     </div>
+//   );
+// };
+// export default Library;
+
+import "./css/Library.css";
+
 import { useState } from "react";
 
 const yogaPoses = [
@@ -32,7 +52,7 @@ const Library = () => {
                 className="search-bar"
             />
             <div className="pose-grid">
-                {!selectedPose && filteredPoses.map((pose) => (
+                {filteredPoses.map((pose) => (
                     <div key={pose.id} className="pose-card" onClick={() => setSelectedPose(pose)}>
                         <img src={pose.image} alt={pose.name} />
                         <h3>{pose.name}</h3>
